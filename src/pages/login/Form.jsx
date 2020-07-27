4a08bd9305a232b3ebad924f3fed7afcbe404637
Evellyn <xe.uyemura@gmail.com>
2020-07-27 15:52:32 -0300

    mudanca nome da variavel

--- src/pages/login/Form.jsx
+++ src/pages/login/Form.jsx
@@ -6,10 +6,9 @@ export default (props) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   
-
-  const setfEmail = (e) => {
-    setEmail(e)
-    props.saveEmail(e)
+  const functionSetEmail = (element) => {
+    setEmail(element)
+    props.saveEmail(element)
   }
 
   const login = (event) => {
@@ -27,7 +26,7 @@ export default (props) => {
         <>
           <form className="form-auth">
               <label>Email</label>
-              <input type="text" value={email} onChange={e => setfEmail(e.target.value)} />
+              <input type="text" value={email} onChange={e => functionSetEmail(e.target.value)} />
               <label>Senha</label>
               <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
               <button type="submit" onClick={login}>Login</button>
