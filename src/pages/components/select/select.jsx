17541ad018be03a44bee4de5d87185a5943657e6
Adelia <adeliacristinecs@gmail.com>
2020-07-28 12:32:28 -0300

    atualização do css para ipad, componentização, firebase não funciona

---
+++ src/pages/components/select/select.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Dropdown} from 'react-bootstrap'
+
+const Select = props =>{
+	return(
+		<DropdownButton name={props.name} className={props.class} onClick={props.click}>
+		<Dropdown.ItemText>{props.title}</Dropdown.ItemText>
+		<Dropdown.Item as="button">{props.title}</Dropdown.Item>
+		<Dropdown.Item as="button">{props.title}</Dropdown.Item>
+	  </DropdownButton>
+		)
+}
+export default Select
+
+
+
+
