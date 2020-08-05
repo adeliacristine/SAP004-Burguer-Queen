49523e2aaca18e8b2b541fc3c4e2b13521b39482
Adelia <adeliacristinecs@gmail.com>
2020-08-05 19:44:07 -0300

    modal para os adicionais prontos

---
+++ src/pages/components/menu/modal.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Modal } from "react-bootstrap";
+import BtnP from '../button/button'
+
+const Options= ()=>{
+	return(
+<h2>Frango
+</h2>
+<h2>Carne
+</h2>
+<h2>Vegetariano
+</h2>
+
+<h1>Adicionar por 1 real?</h1>
+<h2>
+	Queijo 
+</h2>
+<h2>Ovo</h2>
+
+<BtnP>Adicionar</BtnP>
+	)
+}
+export default Options
